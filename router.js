5e3c6ddfa3a9cabd73e9658318b1282edb69cf18
HungryBird <381548325@qq.com>
2020-01-09 01:04:20 +0800

    :bug: 修正找不到路由退出程序

--- router.js
+++ router.js
@@ -5,7 +5,19 @@ const url = require('url');
 function router(request) {
     const pathname = url.parse(request.url).pathname;
     console.log('pathname: ', pathname)
-    const pathURL = path.resolve(__dirname, pathname);
+    let target = '';
+    if (pathname === '/') {
+        target = 'web/index.html';
+    }
+    else if(pathname === '/favicon.ico') {
+        target = 'favicon.ico';
+    }
+    else {
+        target = 'web/' + pathname;
+    }
+
+    console.log('__dirname: ', path.join(__dirname, target))
+    const pathURL = path.resolve(__dirname, target);
     console.log('pathURL: ', pathURL)
     const res = fs.readFileSync(pathURL)
     return res;
